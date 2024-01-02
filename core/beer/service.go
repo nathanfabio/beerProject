35fdcb1a59d137cf39d770d8a7f2e907eccd9d23
Nathan Fabio <nathan_3_m@hotmail.com>
2024-01-02 20:43:22 -0300

    feat: add logic for service

--- core/beer/service.go
+++ core/beer/service.go
@@ -1,2 +1,36 @@
 package beer
 
+
+type UserCase interface {
+	GetAll() ([]*Beer, error)
+	Get(ID int) (*Beer, error)
+	Store(b *Beer) error
+	Update(b *Beer) error
+	Remove(ID int) error
+}
+
+type Service struct {}
+
+func NewService() *Service {
+	return &Service{}
+}
+
+func (s *Service) GetAll() ([]*Beer, error) {
+	return []*Beer{}, nil
+}
+
+func (s *Service) Get(ID int) (*Beer, error) {
+	return &Beer{}, nil
+}
+
+func (s *Service) Store(b *Beer) error {
+	return nil
+}
+
+func (s *Service) Update(b *Beer) error {
+	return nil
+}
+
+func (s *Service) Remove(ID int) error {
+	return nil
+}
